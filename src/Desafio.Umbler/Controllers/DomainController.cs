63eb5d02931972b3a7c797b483f838806a5ee151
Elkerton Rodrigues <elkerton17@gmail.com>
2021-03-04 22:00:55 -0300

    Ajuster no Desafio.Umbler.csproj, sincronizando dotnetcli com VS +  ajustando complexidade do DomainControlle

--- src/Desafio.Umbler/Controllers/DomainController.cs
+++ src/Desafio.Umbler/Controllers/DomainController.cs
@@ -35,45 +35,64 @@ namespace Desafio.Umbler.Controllers
 
                 if (domain == null)
                 {
-                    var response = await WhoisClient.QueryAsync(domainName);
-
-                    var lookup = new LookupClient();
-                    var result = await lookup.QueryAsync(domainName, QueryType.A);
-                    var resultNS = await lookup.QueryAsync(domainName, QueryType.NS);
-
-                    domain = new Domain(domainName, result, resultNS, response.Raw);
-
-                    var hostResponse = await WhoisClient.QueryAsync(domain.Ip);
-
-                    domain.HostedAt = hostResponse.OrganizationName;
+                     domain = await this.SearchDomain(domainName);
 
                     _db.Domains.Add(domain);
                 }
 
                 if (DateTime.Now.Subtract(domain.UpdatedAt).TotalMinutes > domain.Ttl)
                 {
-                    var response = await WhoisClient.QueryAsync(domainName);
+                    domain = await this.SearchDomain(domainName);
 
-                    var lookup = new LookupClient();
-                    var result = await lookup.QueryAsync(domainName, QueryType.A);
-                    var resultNS = await lookup.QueryAsync(domainName, QueryType.NS);
+                    _db.Domains.Update(domain);
 
-                    domain = new Domain(domainName, result, resultNS, response.Raw);
-
-                    var hostResponse = await WhoisClient.QueryAsync(domain.Ip);
-
-                    domain.HostedAt = hostResponse.OrganizationName;
-                    domain.UpdatedAt = DateTime.Now;
                 }
 
-                    await _db.SaveChangesAsync();
-             }
+                await _db.SaveChangesAsync();
+            }
             catch (Exception e)
             {   
                 throw e;
             }
-           
-            return Ok( new { Name = domain.Name, Ip = domain.Ip, HostedAt = domain.HostedAt, WhoIs = domain.WhoIs, NsRecords = domain.GetNsList()});
+
+            var responseBody = new
+            {
+                Name = domain.Name,
+                Ip = domain.Ip,
+                HostedAt = domain.HostedAt,
+                WhoIs = domain.WhoIs,
+                NsRecords = domain.GetNsList()
+            };
+
+            return Ok(responseBody);
+        }
+
+        public async Task<Domain> SearchDomain(string domainName)
+        {
+            var domain = new Domain();
+
+            try
+            {
+                var response = await WhoisClient.QueryAsync(domainName);
+
+                var lookup = new LookupClient();
+                var result = await lookup.QueryAsync(domainName, QueryType.A);
+                var resultNS = await lookup.QueryAsync(domainName, QueryType.NS);
+
+                domain = new Domain(domainName, result, resultNS, response.Raw);
+
+                var hostResponse = await WhoisClient.QueryAsync(domain.Ip);
+
+                domain.HostedAt = hostResponse.OrganizationName;
+                domain.UpdatedAt = DateTime.Now;
+
+                return domain;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
         }
     }
 }
