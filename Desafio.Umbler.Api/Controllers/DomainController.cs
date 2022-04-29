13901d5b31ed4fe52a846fa92a62e850b72dbe4e
João <joaos_vb@hotmail.com>
2022-04-28 22:12:57 -0300

    Cria camada de Api

---
+++ Desafio.Umbler.Api/Controllers/DomainController.cs
@@ -0,0 +1,24 @@
+﻿using Desafio.Umbler.Dominio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio.Umbler.Api.Controllers
+{
+    [Route("api")]
+    public class DomainController : Controller
+    {
+        private readonly IDomainService _domainService;
+
+        public DomainController(IDomainService domainService)
+        {
+            _domainService = domainService;
+        }
+
+        [HttpGet, Route("domain/{domainName}")]
+        public async Task<IActionResult> Get(string domainName)
+        {
+            var domain = await _domainService.Get(domainName);
+
+            return Ok(domain);
+        }
+    }
+}
---
+++ Desafio.Umbler.Api/Views/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Desafio.Umbler.Api.Views
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
